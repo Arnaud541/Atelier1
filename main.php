8f4543f63dd69e95a420ad0128f2b9ff87be7338
Morgan <morganpetit5@gmail.com>
2022-11-08 17:18:01 +0100

    Idée d'Arnaud encore une fois

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+$data = parse_ini_file("config/config.ini");
+
+$db = new Illuminate\Database\Capsule\Manager();
+
+$db->addConnection($data); /* configuration avec nos paramètres */
+$db->setAsGlobal();            /* rendre la connexion visible dans tout le projet */
+$db->bootEloquent(); /* établir la connexion */
+
+print_r($db);
