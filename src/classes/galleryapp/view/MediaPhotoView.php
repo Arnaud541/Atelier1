078f59f839fd5a7ffee2e76e3f2358ae805250af
Morgan <morganpetit5@gmail.com>
2022-11-09 14:19:59 +0100

    J'ai besoin de pull

--- src/classes/galleryapp/view/MediaPhotoView.php
+++ src/classes/galleryapp/view/MediaPhotoView.php
@@ -8,6 +8,7 @@ class MediaPhotoView{
         ?>
             <nav id="navbar">
                 <a href="#">MediaPhoto</a>
+                
                 <a href="#">Inscription</a>
                 <a href="#">Connexion</a>
             </nav>
