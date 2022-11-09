2adb5899cb7e698444b3cb8e60e161c38b608bf6
SkyFlyOne <pau.gillon0@gmail.com>
2022-11-09 14:17:02 +0100

    ajout br signupview

--- src/classes/galleryapp/view/SignupView.php
+++ src/classes/galleryapp/view/SignupView.php
@@ -10,12 +10,12 @@ class SignupView extends MediaPhotoView implements Renderer
     public function render(): string
     {
         return '<form action="" method="post">
-                    <h1>Inscription</h1>
-                    <input type="text" name="firstname">
-                    <input type="text" name="lastname">
-                    <input type="text" name="pseudo">
-                    <input type="text" name="password">
-                    <input type="text" name="confPass">
+                    <h1>Inscription</h1><br/>
+                    <input type="text" name="firstname"><br/>
+                    <input type="text" name="lastname"><br/>
+                    <input type="text" name="pseudo"><br/>
+                    <input type="text" name="password"><br/>
+                    <input type="text" name="confPass"><br/>
                     <button type="submit">S inscrire</button>
                 </form>';
     }
