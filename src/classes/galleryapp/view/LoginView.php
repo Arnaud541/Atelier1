15b879c0441552d49a33423b627bfdece639d4f4
Morgan <morganpetit5@gmail.com>
2022-11-09 13:12:54 +0100

    Ajout du fichier CSS et début de la navbar

---
+++ src/classes/galleryapp/view/LoginView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MediaPhoto\galleryapp\view;
+
+class LoginView {
+
+    public function __construct(){
+        
+    }
+
+}
---
+++ src/classes/galleryapp/view/MediaPhotoView.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MediaPhoto\galleryapp\model;
+
+class MediaPhotoView{
+
+    public function renderNavbar(){
+        ?>
+            <nav id="navbar">
+                <a href="#">MediaPhoto</a>
+                <a href="#">Inscription</a>
+                <a href="#">Connexion</a>
+            </nav>
+        <?php
+    }
+
+}
