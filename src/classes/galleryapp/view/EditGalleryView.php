5b91d91f29bb41241d0b3de221bf23eb0ab355b5
AndriyLeb <andriy.lebedev4@etu.univ-lorraine.fr>
2022-11-10 17:33:44 +0100

    push EditGalleryView

---
+++ src/classes/galleryapp/view/EditGalleryView.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MediaPhoto\galleryapp\view;
+
+use MediaPhoto\mf\view\Renderer;
+use MediaPhoto\galleryapp\view\MediaPhotoView;
+
+class EditGalleryView extends MediaPhotoView implements Renderer
+{
+    public function render(): string
+    {
+
+        $images = $this->data;
+        $galleryName = $images[0]->gallery()->first()->name;
+
+        $content = "<div>
+                        <h2>$galleryName</h2>
+                        <div>";
+        if (count($images) == 0) {
+            $content .= "<h1>Aucune image dans la galerie</h1>";
+        } else {
+
+            foreach ($images as $image) {
+
+                $id = $image->id;
+                $url_image = $this->router->urlFor("image_view", ['id' => $id]);
+                $url_delet = $this->router->urlFor("delet_image", ['id' => $id]);
+
+
+                //$url_edit = $this->router->urlFor("Quand ce sera fait");
+                $content .= "<div>
+                                <a  href=''>Edit</a>
+                                <a  href='$url_delet'>Delet</a>
+                            </div>";
+                $content .= "<div><a href='$url_image'><img alt={$image->title} src={$image->path}></a></div>";
+            }
+            $content .= "</div></div>";
+        }
+
+        return $content;
+    }
+}
