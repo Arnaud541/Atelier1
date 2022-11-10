2e3f365d1c6ec1e79a36aa30d43ad69186198ad4
SkyFlyOne <pau.gillon0@gmail.com>
2022-11-10 15:50:42 +0100

    Footer en bas pour de vrai

--- src/classes/galleryapp/view/SignupView.php
+++ src/classes/galleryapp/view/SignupView.php
@@ -2,21 +2,24 @@
 
 namespace MediaPhoto\galleryapp\view;
 
-use MediaPhoto\galleryapp\view\MediaPhotoView;
 use MediaPhoto\mf\view\Renderer;
+use MediaPhoto\mf\view\AbstractView;
+use MediaPhoto\galleryapp\view\MediaPhotoView;
 
 class SignupView extends MediaPhotoView implements Renderer
 {
     public function render(): string
-    {
+    {     
         return '<form action="" method="post">
                     <h1>Inscription</h1><br/>
-                    <input type="text" name="lastname" placeholder="Nom"><br/>
-                    <input type="text" name="firstname" placeholder="Prenom"><br/>
-                    <input type="text" name="pseudo" placeholder="Nom d\'utilisateur"><br/>
-                    <input type="text" name="password" placeholder="Mot de passe"><br/>
-                    <input type="text" name="confPass" placeholder="Confirmer le mot de passe"><br/>
-                    <button type="submit">S\'inscrire</button>
+                    <div id="pos">
+                        <input type="text" name="lastname" placeholder="Nom"><br/>
+                        <input type="text" name="firstname" placeholder="Prenom"><br/>
+                        <input type="text" name="pseudo" placeholder="Nom d\'utilisateur"><br/>
+                        <input type="text" name="password" placeholder="Mot de passe"><br/>
+                        <input type="text" name="confPass" placeholder="Confirmer le mot de passe"><br/>
+                        <button type="submit">S\'inscrire</button>
+                    </div>
                 </form>';
     }
 }
